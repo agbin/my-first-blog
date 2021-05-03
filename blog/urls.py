4b6d59ad5ddedb4987f94a70de9196f9678fc1c4
agbin <op@wp.pl>
2021-05-03 22:06:29 +0200

    Zmieniony kod HTML na stronie.

--- blog/urls.py
+++ blog/urls.py
@@ -5,3 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name="post_list" ),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
